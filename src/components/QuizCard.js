93cc7885592446df9f942d4ed063ab94e64c3e25
KonstantinBYC <globalmacintosh@gmail.com>
2023-08-20 16:34:55 +0300

    QCard edit

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,12 +1,12 @@
-export const QuizCard = ({ quiz: { topic, level, time, question } }) => {
-  return (
-    <div>
-      <h2>{quiz.topic} </h2>
-      <div>
-        <p>Level: {quiz.level}</p>
-        <p>Time : {quiz.time}</p>
-        <p>Question: {quiz.question}</p>
-      </div>{' '}
-    </div>
-  );
-};
+// export const QuizCard = ({ quiz: { topic, level, time, question } }) => {
+//   return (
+//     <div>
+//       <h2>{quiz.topic} </h2>
+//       <div>
+//         <p>Level: {quiz.level}</p>
+//         <p>Time : {quiz.time}</p>
+//         <p>Question: {quiz.question}</p>
+//       </div>{' '}
+//     </div>
+//   );
+// };
